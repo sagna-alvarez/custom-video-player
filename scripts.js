ba7bdb2387db65fb4758c5272900f10ee5c6d8aa
Sagna macbook <sanat.tktl@gmail.com>
2023-12-13 19:11:51 +0600

    keep doing it

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player__slider');
+
+console.log(player);
+console.log(video);
+console.log(progress);
+console.log(progressBar);
+console.log(toggle);
+console.log(skipButtons);
+console.log(ranges);
